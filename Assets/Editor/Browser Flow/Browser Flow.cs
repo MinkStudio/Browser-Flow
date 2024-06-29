6efeb4b7e76418c0ea8f31c04967ababd5920287
Mink-studio <nimaabdoli650@gmail.com>
2024-06-29 18:49:51 +0330

    Get and set grid size after open and close

--- Assets/Editor/Browser Flow/Browser Flow.cs
+++ Assets/Editor/Browser Flow/Browser Flow.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class BrowserFlow : EditorWindow
 {
+    private const string GridSizeKey = "ProjectWindow_GridSize";
+
 
     [MenuItem("Tools/Browser Flow/Browser Flow")]
     public static void ShowWindow()
@@ -13,8 +15,6 @@ public class BrowserFlow : EditorWindow
         BrowserFlow BrowserFlow_window = (BrowserFlow)GetWindow(typeof(BrowserFlow));
         BrowserFlow_window.titleContent = new GUIContent("Browser Flow");
         BrowserFlow_window.Show();
-
-        
     }
 
     private void OnGUI()
@@ -32,7 +32,11 @@ public class BrowserFlow : EditorWindow
         GUILayout.Label("\n\n This window allows you to manipulate the Project Browser.", EditorStyles.wordWrappedLabel);
     }
 
-    // toggle state of project window. and needed to have hotkey in unity editor
+    #region State of Project Window
+
+    /// <summary>
+    /// toggle state of project window. and needed to have hot-key in unity editor
+    /// </summary>
     [MenuItem("Tools/Browser Flow/Open-close &q")]
     static private void OpenCloseProjectBrowser()
     {
@@ -47,7 +51,10 @@ public class BrowserFlow : EditorWindow
 
     }
 
-    // check if project window is open or close.
+    /// <summary>
+    /// check if project window is open or close.
+    /// </summary>
+    /// <returns> Return State(Open/Close) of Project Window</returns>
     private static bool IsProjectWindowOpen()
     {
         System.Type projectBrowserType = System.Type.GetType("UnityEditor.ProjectBrowser, UnityEditor");
@@ -59,10 +66,18 @@ public class BrowserFlow : EditorWindow
 
         // check how many project window is open and if there is none, presumed window is close. it has some issue if there is more than one project window is opened.
         EditorWindow[] windows = Resources.FindObjectsOfTypeAll(projectBrowserType) as EditorWindow[];
-        //Debug.Log(windows.Length);
+
+        
         return windows != null && windows.Length > 0;
     }
 
+    #endregion
+
+    #region Close Project Window 
+
+    /// <summary>
+    /// Close project window and save it's grid size and last path user was in in editor preference
+    /// </summary>
     static private void CloseProjectBrowser()
     {
         //Get the Project Browser window
@@ -80,10 +95,31 @@ public class BrowserFlow : EditorWindow
             return;
         }
 
+        // get and save grid size of project window
+        var GridSizeField = ProjectWindowType.GetField("m_StartGridSize", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        if (GridSizeField != null) // check to be sure grid size not empty and check if project window exist
+        {
+            int GridSize = (int)GridSizeField.GetValue(ProjectWindow);
+            EditorPrefs.SetInt(GridSizeKey, GridSize);
+        }
+        else
+        {
+            Debug.Log("m_StartGridSize is not found");
+        }
+
+        //close the window
         ProjectWindow.Close();
 
-    }
+    }// CloseProjectBrowser function
 
+    #endregion 
+
+
+    #region Open Project Window
+
+    /// <summary>
+    /// open(show) Project window and set last grid size that user choose and go to last path user was in.
+    /// </summary>
     private static void OpenProjectWindow()
     {
         // Get the Project Browser window
@@ -102,9 +138,24 @@ public class BrowserFlow : EditorWindow
             return;
         }
 
+        //Set Grid size 
+        var GridSizeField = ProjectWindowType.GetField("m_StartGridSize", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        if (GridSizeField != null)
+        {
+            int iconSize = EditorPrefs.GetInt(GridSizeKey, 16); // Default icon size is 16
+            GridSizeField.SetValue(ProjectWindow, iconSize);
+        }
+        else
+        {
+            Debug.Log("m_StartGridSize is not found");
+        }
+
+
         ProjectWindow.Show();
         
     }
+    #endregion
+
 }
 
 

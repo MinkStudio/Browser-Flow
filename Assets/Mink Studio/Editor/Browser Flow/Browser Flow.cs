5d56e612573b5c918a762a05156c29cc8bf28c8f
MiNK | Studio <122664866+MinkStudio@users.noreply.github.com>
2026-07-16 17:20:42 +0330

    code cleanup

--- Assets/Mink Studio/Editor/Browser Flow/Browser Flow.cs
+++ Assets/Mink Studio/Editor/Browser Flow/Browser Flow.cs
@@ -7,28 +7,35 @@ namespace BrowserFlow
 {
     public class BrowserFlow : EditorWindow
     {
-        // keep the key for Editor preference.
+        // Keep the key for Editor preference to save last grid size user choosed.
         private const string GridSizeKey = "ProjectWindow_GridSize";
+        // Keep the key for Editor preference to save last folder path user was in.
         private const string LastFolderKey = "ProjectWindow_LastFolder";
 
+        // Prevent project window from closing when user want to keep it open.
         private static bool FocusTrigger = true;
 
+        // To track focus state of project window.
         private static ProjectBrowserFocus ProjectWinState;
+        // Instanc of EditorWindow type to be populated with project window instance.
         private static EditorWindow ProjectWindow;
 
-        // class constructor
+
+        #region Constructor
+        // Class constructor of Browser flow script
         static BrowserFlow()
         {
-            // make Scriptable object to can track focus change even when window close
+            // Make Scriptable object to can track focus change even when window close
             ProjectWinState = ScriptableObject.CreateInstance<ProjectBrowserFocus>();
 
-            // create and initiate update event (i think it's for unity editor itself) that update with each frame.
+            // Create and initiate update event (i think it's for unity editor itself) that update with each frame.
             EditorApplication.update += UIUpdate;
         }
+        #endregion
 
         // placeholder at this point 
         // Not needed yet. it doesn't needed yet maybe for future update that i want add setting page.
-        // [MenuItem("Tools/Browser Flow/Browser Flow")]
+        //[MenuItem("Tools/Browser Flow/Browser Flow")]
         public static void ShowWindow()
         {
             // Show the custom window
@@ -40,9 +47,9 @@ namespace BrowserFlow
         #region State of Project Window
 
         /// <summary>
-        /// toggle state of project window. and needed to have hot-key in unity editor
+        /// Toggle open/close state of project window. and it have hot-key(ctrl+spcae) in unity editor
         /// </summary>
-        [MenuItem("Tools/Browser Flow/1 - Open-Close %SPACE")]
+        [MenuItem("Tools/Browser Flow/1 - Open-Close %SPACE")] // add sub menu in Unity editor in Tools menu that run function below
         private static void OpenCloseProjectBrowser()
         {
             if (IsProjectWindowOpen())
@@ -56,7 +63,7 @@ namespace BrowserFlow
         }
 
         /// <summary>
-        /// if user want to keep project window open and don't close it 
+        /// If user want to keep project window open and don't be close when project window lost focus
         /// </summary>
         [MenuItem("Tools/Browser Flow/2 - Focus On-Off %q")]
         private static void FocusLoseHandler()
@@ -70,8 +77,11 @@ namespace BrowserFlow
         /// <returns> Return State(Open/Close) of Project Window</returns>
         private static bool IsProjectWindowOpen()
         {
+            // Unity editor limitions that i have to use reflection to get project window type because it's not public class. so i have to get it by name.
+            // there was limition that(can't remember the reason) i can't hide project menu so each time i have to close it or after each opening there was added to number of project window that open.
+            // maybe my code is wrong, must check it again after that i finded solution to set path for project windows
             System.Type projectBrowserType = System.Type.GetType("UnityEditor.ProjectBrowser, UnityEditor");
-            if (projectBrowserType == null)
+            if (projectBrowserType == null) // most likely not going to happen.
             {
                 Debug.LogError("ProjectBrowser type not found");
                 return false;
@@ -87,20 +97,22 @@ namespace BrowserFlow
 
         #region Open Project Window
         /// <summary>
-        /// open(show) Project window and set last grid size that user choose
+        /// Open(show) Project window and set last grid size that user choose and set last folder user visit
         /// </summary>
         public static void OpenProjectWindow()
         {
             // Get the Project Browser window
             System.Type ProjectWindowType = System.Type.GetType("UnityEditor.ProjectBrowser, UnityEditor");
 
-            if (ProjectWindowType == null)
+            // i pretty sure no need of this. i think this was to find name of fields that used by unity editor for project window and make sure of existence of it
+            // maybe it's better to keep it because maybe in future or old versio of unity they change it
+            if (ProjectWindowType == null) 
             {
                 Debug.LogError("ProjectBrowser type not found");
                 return;
             }
 
-
+            //this line get instace of project window
             ProjectWindow = EditorWindow.GetWindow(ProjectWindowType);
             if (ProjectWindow == null)
             {
@@ -120,12 +132,15 @@ namespace BrowserFlow
                 Debug.Log("m_StartGridSize is not found");
             }
 
+            //TODO : i should check what will happend if i put this after file path setter
             ProjectWindow.Show();
 
-            // set last visited folder
+            // Set last visited folder
             string savedFolder = EditorPrefs.GetString(LastFolderKey, "");
-            if (!string.IsNullOrEmpty(savedFolder) && AssetDatabase.IsValidFolder(savedFolder))
+            // this statement make sure savedfolder string be actual folder path not path to seleceted file or empty string. and check if folder is valid folder in project.
+            if (!string.IsNullOrEmpty(savedFolder) && AssetDatabase.IsValidFolder(savedFolder)) // AssetDatabase : it's the API that manages everything about your project's Assets folder
             {
+                // make the get id if folder in unity.
                 var obj = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(savedFolder);
                 if (obj != null)
                 {
@@ -144,6 +159,9 @@ namespace BrowserFlow
                         object entityId = implicitOp.Invoke(null, new object[] { id });
 
                         var pw = ProjectWindow;
+
+                        // delay by a tick in editor to make sure "showFolderContents" window layout be ready
+                        //TODO : check without delay to see how relible it is or what will happened
                         EditorApplication.delayCall += () =>
                         {
                             showFolderContents.Invoke(pw, new object[] { entityId, true });
@@ -158,11 +176,11 @@ namespace BrowserFlow
 
         #region Close Project Window 
         /// <summary>
-        /// Close project window and save it's grid size and last path user was in in editor preference
+        /// Close project window and save it's grid size and last folder user was in.
         /// </summary>
         private static void CloseProjectBrowser()
         {
-            //Get the Project Browser window
+            //Get the Project Browser window type
             System.Type ProjectWindowType = System.Type.GetType("UnityEditor.ProjectBrowser, UnityEditor");
             if (ProjectWindowType == null)
             {
@@ -170,6 +188,7 @@ namespace BrowserFlow
                 return;
             }
 
+            //get Project window instance
             ProjectWindow = EditorWindow.GetWindow(ProjectWindowType);
             if (ProjectWindow == null)
             {
@@ -177,7 +196,7 @@ namespace BrowserFlow
                 return;
             }
 
-            // get and save grid size of project window
+            // Get and save grid size of project window
             var GridSizeField = ProjectWindowType.GetField("m_StartGridSize", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
             if (GridSizeField != null) // check to be sure grid size not empty and check if project window exist
             {
@@ -189,12 +208,14 @@ namespace BrowserFlow
                 Debug.Log("m_StartGridSize is not found");
             }
 
-            var getActiveFolderPath = ProjectWindowType.GetMethod("GetActiveFolderPath",System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+            //get last folder user was in and save it to EditorPrefs2
+            var getActiveFolderPath = ProjectWindowType.GetMethod("GetActiveFolderPath"
+                ,System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
 
             if (getActiveFolderPath != null)
             {
                 string activeFolder = getActiveFolderPath.Invoke(ProjectWindow, null) as string;
-                Debug.Log($"[BrowserFlow] GetActiveFolderPath = '{activeFolder}'");
+                //Debug.Log($"[BrowserFlow] GetActiveFolderPath = '{activeFolder}'");
                 if (!string.IsNullOrEmpty(activeFolder))
                 {
                     EditorPrefs.SetString(LastFolderKey, activeFolder);
@@ -202,7 +223,6 @@ namespace BrowserFlow
             }
 
             ProjectWindow.Close();
-            //ProjectWindow = null;
         }
 
         #endregion
@@ -210,12 +230,11 @@ namespace BrowserFlow
         #region UI Update
 
         /// <summary>
-        /// This is event that fire with each UI update
+        /// This is event that fire with each UI update(editor UI fps)
         /// </summary>
         private static void UIUpdate()
         {
-
-            // check for if project window has focus or not if not close project window
+            // Check for if project window has focus or not if not close project window
             if (ProjectWindow != null)
             {
                 if (!ProjectWindow.IsFocused() && ProjectWinState.FocusedCheck && FocusTrigger)
@@ -228,12 +247,11 @@ namespace BrowserFlow
 
     }
 
-
     public class ProjectBrowserFocus : ScriptableObject
     {
         public bool FocusedCheck = false;
     }
 
 
-    // '%' for CTRL (or CMD on macOS) | '#; for SHIFT | '&' for ALT | '_' for no modifiers like _g it's only g as hotkey
-}
+    //TODO : '%' for CTRL (or CMD on macOS) | '#; for SHIFT | '&' for ALT | '_' for no modifiers like _g it's only g as hotkey
+}   //TODO : if more than one projet window opened, they will close one by one first.
